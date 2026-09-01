3822fe8ac85603630d078005067afb5ac861867a
dereckalexander1313-commits <dereckalexander1313@gmail.com>
2026-09-01 15:13:33 -0600

    Update App.swift

--- ThreeOneOSFive/App.swift
+++ ThreeOneOSFive/App.swift
@@ -2,20 +2,9 @@ import SwiftUI
 
 @main
 struct ThreeOneOSFiveApp: App {
-    init() { copyAll3105() }
     var body: some Scene {
-        WindowGroup { ContentView() }
-    }
-    func copyAll3105() {
-        let fm = FileManager.default
-        guard let docs = fm.urls(for:.documentDirectory, in:.userDomainMask).first else { return }
-        let patchesDir = docs.appendingPathComponent("Patches", isDirectory: true)
-        try? fm.createDirectory(at: patchesDir, withIntermediateDirectories: true)
-        if let resources = Bundle.main.urls(forResourcesWithExtension: "3105", subdirectory: nil) {
-            for file in resources {
-                let dest = patchesDir.appendingPathComponent(file.lastPathComponent)
-                if!fm.fileExists(atPath: dest.path) { try? fm.copyItem(at: file, to: dest) }
-            }
+        WindowGroup {
+            ContentView()
         }
     }
 }

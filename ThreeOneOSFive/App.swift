84b20489e59323801268b6feecc7536d5de9092b
dereckalexander1313-commits <dereckalexander1313@gmail.com>
2026-09-01 14:54:39 -0600

    Update App.swift

--- ThreeOneOSFive/App.swift
+++ ThreeOneOSFive/App.swift
@@ -1,43 +1,21 @@
 import SwiftUI
 
 @main
-struct YourAppNameApp: App {
-    init() {
-        copyBundledPatches()
-        setupPurpleAppearance()
-    }
-
+struct ThreeOneOSFiveApp: App {
+    init() { copyAll3105() }
     var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
+        WindowGroup { ContentView() }
     }
-
-    func copyBundledPatches() {
-        let fileManager = FileManager.default
-        guard let docsURL = fileManager.urls(for: .documentDirectory, in: .userDomainMask).first else { return }
-        let patchesDir = docsURL.appendingPathComponent("Patches", isDirectory: true)
-        try? fileManager.createDirectory(at: patchesDir, withIntermediateDirectories: true)
-
-        let bundledPatches = [
-            "com.ea.ios.simsmobile_5912fbb3-9a69-4f00-bd38-1c0ca6d04b4c_v7_9.7.9.139048.3105",
-            "com.ea.ios.simsmobile_5912fbb3-9a69-4f00-bd38-1c0ca6d04b4c_v7_9.7.0.139048.3105",
-            "com.ea.ios.simsmobile_5912fbb3-9a69-4f00-bd38-1c0ca6d04b4c_v7_9.7.1.139048.3105"
-        ]
-
-        for patchName in bundledPatches {
-            if let bundleURL = Bundle.main.url(forResource: patchName, withExtension: nil) {
-                let destURL = patchesDir.appendingPathComponent(patchName)
-                if !fileManager.fileExists(atPath: destURL.path) {
-                    try? fileManager.copyItem(at: bundleURL, to: destURL)
-                }
+    func copyAll3105() {
+        let fm = FileManager.default
+        guard let docs = fm.urls(for:.documentDirectory, in:.userDomainMask).first else { return }
+        let patchesDir = docs.appendingPathComponent("Patches", isDirectory: true)
+        try? fm.createDirectory(at: patchesDir, withIntermediateDirectories: true)
+        if let resources = Bundle.main.urls(forResourcesWithExtension: "3105", subdirectory: nil) {
+            for file in resources {
+                let dest = patchesDir.appendingPathComponent(file.lastPathComponent)
+                if!fm.fileExists(atPath: dest.path) { try? fm.copyItem(at: file, to: dest) }
             }
         }
-    }
-
-    func setupPurpleAppearance() {
-        UINavigationBar.appearance().tintColor = UIColor.purple
-        UITabBar.appearance().tintColor = UIColor.purple
-        UIButton.appearance().tintColor = UIColor.purple
     }
 }
